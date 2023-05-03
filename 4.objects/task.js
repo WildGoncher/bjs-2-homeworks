7b2dac000d753fc7653883c3a8d1aae5e4f06de0
WildG <wildgoncher@gmail.com>
2023-05-03 22:38:02 +0300

    Fixed errors, solved remaining exercises

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,34 +1,44 @@
 function Student(name, gender, age) {
-    function Student(name, gender, age) {
-        this.name = name,
-        this.gender = gender,
-        this.age = age,
-        this.marks = []
-    }
-    
-    let student1 = new Student("Alex", "male", 31);
-    let student2 = new Student("Sandra", "female", 30);
-    let student3 = new Student("Mike", "male", 29);
-    let student4 = new Student("Boris", "male", 69);
+    (this.name = name),
+    (this.gender = gender),
+    (this.age = age),
+    (this.marks = []);
 }
+
+let student1 = new Student("Alex", "male", 31);
+let student2 = new Student("Sandra", "female", 30);
+let student3 = new Student("Mike", "male", 29);
+let student4 = new Student("Boris", "male", 69);
 
 Student.prototype.setSubject = function (subjectName) {
-    return this.subject = subjectName
-}
+  return (this.subject = subjectName);
+};
 let student = new Student("Caleb", "male", 24);
-student.setSubject("A");  
-
+student.setSubject("A");
 
 Student.prototype.addMarks = function (...marks) {
-    if (this.marks !== undefined) {
-        return this.marks.push(...marks)
-    }
-}
+  if (this.marks !== undefined) {
+    return this.marks.push(...marks);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  let sum = 0;
+  let l = 0;
+  if (this.marks !== undefined) {
+    l = this.marks.length;
+  }
+  if (l === 0) {
+    return 0;
+  }
+  for (let i = 0; i < l; i++) {
+    sum = sum + Number(this.marks[i]);
+  }
+  return sum / l;
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  delete this.subject;
+  delete this.marks;
+  return (this.excluded = reason);
+};
