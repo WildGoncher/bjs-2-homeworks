cad1ff9199ae2184648d7adf04ca7a3f95c7de39
WildGoncher <wildgoncher@gmail.com>
2023-05-03 12:03:04 +0300

    Three of of five solved

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,13 +1,28 @@
 function Student(name, gender, age) {
-  
+    function Student(name, gender, age) {
+        this.name = name,
+        this.gender = gender,
+        this.age = age,
+        this.marks = []
+    }
+    
+    let student1 = new Student("Alex", "male", 31);
+    let student2 = new Student("Sandra", "female", 30);
+    let student3 = new Student("Mike", "male", 29);
+    let student4 = new Student("Boris", "male", 69);
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    return this.subject = subjectName
 }
+let student = new Student("Caleb", "male", 24);
+student.setSubject("A");  
+
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks !== undefined) {
+        return this.marks.push(...marks)
+    }
 }
 
 Student.prototype.getAverage = function () {
