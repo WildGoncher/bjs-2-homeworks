f58f570f69fbf06a4d1941448c4084ee66e77696
WildG <wildgoncher@gmail.com>
2023-05-10 23:21:47 +0300

    Ex 1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,24 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+  function wrapper(...args) {
+    const hash = md5(args);
+    let objectInCache = cache.find((item) => item.hash === hash);
+    let index = cache.findIndex((arr) => arr.hash === hash);
+    if (objectInCache) {
+      console.log(`Из кэша: ${cache[index].result}`);
+      return `Из кэша: ${cache[index].result}`;
+    }
+    let result = func(...args);
+    cache.push({ hash, result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
-}
+function debounceDecoratorNew(func, delay) {}
